270e3b72e983303ea0111a78de9c134f102f9d75
s酱 <96040102+scatyf3@users.noreply.github.com>
2023-12-14 06:07:01 +0000

    finish phase 2

--- setup.py
+++ setup.py
@@ -3,10 +3,23 @@ import sysconfig
 
 def main():
     CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
-    LDFLAGS = ['-fopenmp']
-    # Use the setup function we imported and set up the modules.
-    # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
-    # TODO: YOUR CODE HERE
+    LDFLAGS = ['-L/usr/lib/llvm-10/lib','-fopenmp']
+    
+    module1 = Extension('numc',
+                    include_dirs=['matrix.h'],
+                    extra_link_args=LDFLAGS,
+                    sources=['numc.c','matrix.c'],
+                    extra_compile_args=CFLAGS)
+    '''
+    我们要做的不是include_dirs = ['/usr/local/include'],
+                libraries = ['tcl83'],
+                library_dirs = ['/usr/local/lib'],
+    这些文档里的用例, 居然只需要extra_compile_args,extra_link_args，感觉很神奇
+    '''
+    setup(name="numc",
+          version='1.0',
+          description='This is a demo package',
+          ext_modules=[module1])  
 
 if __name__ == "__main__":
     main()
---
+++ test_phase2_correctness.py
@@ -0,0 +1,7 @@
+from numc import Matrix
+
+import numc
+numc.Matrix
+
+import numc as nc
+nc.Matrix
